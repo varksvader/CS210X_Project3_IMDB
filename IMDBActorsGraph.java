ed8237eef4d58ca6ac1c105b0d50870b83111a95
NilsBacke <nils.backe@gmail.com>
2017-11-09 21:25:28 -0500

    started parsing

--- IMDBActorsGraph.java
+++ IMDBActorsGraph.java
@@ -34,17 +34,47 @@ public class IMDBActorsGraph implements Graph {
     public IMDBActorsGraph(String actorsFilename, String actressesFilename) throws IOException {
         actorsScanner = new Scanner(new File(actorsFilename), "ISO-8859-1");
         actressesScanner = new Scanner(new File(actressesFilename), "ISO-8859-1");
-        actors = new ArrayList<>();
+        actors = new ArrayList<ActorNode>();
     }
 
     private void parseData() {
-        
+
+        Boolean copyrightInfoDone = false;
+
+        while (actorsScanner.hasNextLine()) {
+
+            String line = actorsScanner.nextLine();
+
+            // Skips the first few hundred line with the copyright information
+            if (!copyrightInfoDone) {
+                if (line.equals("----			------")) {
+                    copyrightInfoDone = true;
+                }
+                continue;
+            }
+
+            String name = line.substring(0, line.indexOf("\t"));
+            final List<String> movies = new ArrayList<>();
+            String firstMovie = line.substring(line.indexOf("\t") + 1, line.indexOf(")") + 1);
+            movies.add(firstMovie);
+
+            while (!actorsScanner.nextLine().equals("")) {
+                line = actorsScanner.nextLine();
+                String movie = line.substring(0, line.indexOf(")") + 1);
+                movies.add(movie);
+            }
+
+            // actors.add(new ActorNode(name, movies));
+
+        }
+
+        actorsScanner.close();
     }
 
 
     @Override
     public Collection<? extends Node> getNodes() {
-        return null;
+        return actors;
     }
 
     @Override
