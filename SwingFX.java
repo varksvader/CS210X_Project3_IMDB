a3a30cef3c88fe20b5563a437c5cd9ec88fe73e5
emma beeler <emmambeeler@gmail.com>
2017-11-12 18:28:05 -0500

    added the class that lets us run javafx in eclipse
    you still need to adjust the properties of the file as listed on canvas

---
+++ SwingFX.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * TODO: delete this later
+ * this allows u to run JavaFX applications in eclipse without downloading
+ * ec(fx)lipse or whatever is is
+ * additionally, you need to open up properties for the project, build path -> jre
+ * then go to the asset labelled rules and add a new rule with allowing javafx/**
+ * or something like that its on canvas
+ *
+ */
+public class SwingFX extends Application {
+	
+	@Override
+	public void start(Stage stage){
+		// ...
+	}
+	
+	public static void main(String[] args){
+		launch(args);
+	}
+}
