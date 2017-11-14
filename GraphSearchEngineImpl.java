262c8ba1a252b4a1b6d172c2ed918834f4341bfd
Varnika Sinha <varnsinha@gmail.com>
2017-11-13 22:06:30 -0500

    Finished findShortestPath()

--- GraphSearchEngineImpl.java
+++ GraphSearchEngineImpl.java
@@ -1,15 +1,49 @@
-//Blank bfs search for testing
-import java.util.List;
+import java.util.*;
 
 public class GraphSearchEngineImpl implements GraphSearchEngine {
 
-    //implied constructor
-    public GraphSearchEngineImpl() {
-
-    }
+	//Implied constructor
 
     @Override
     public List<Node> findShortestPath(Node s, Node t) {
-        return null;
+    	 List<Node> shortestPathList = new ArrayList<Node>();
+         List<Node> visited = new ArrayList<Node>();
+         Queue<Node> toVisit = new LinkedList<Node>();
+         Stack<Node> pathStack = new Stack<Node>();
+         if (s.equals(t)) {
+             return null;
+         }
+         toVisit.add(s);
+         pathStack.add(s);
+         visited.add(s);
+         Node u = null;
+         while( !toVisit.isEmpty()) {
+             u = toVisit.poll();
+             for(Node neighbor : u.getNeighbors()) {
+                 if(!visited.contains(neighbor)) {
+                     toVisit.add(neighbor);
+                     visited.add(neighbor);
+                     pathStack.add(neighbor);
+                     if(u == t)
+                         break;
+                 }
+             }
+         }
+
+         //To find the path
+         Node node = null;
+         Node currentSrc = t;
+         shortestPathList.add(t);
+         while(!pathStack.isEmpty()) {
+             node = pathStack.pop();
+             if(currentSrc.getNeighbors().contains(node)) {
+                 shortestPathList.add(node);
+                 currentSrc = node;
+                 if(node == s)
+                     break;
+             }
+         }
+
+         return shortestPathList;
     }
 }
