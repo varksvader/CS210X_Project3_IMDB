952b7c0cfbcdcdbe18329cdd16b4f5723b568a5f
Varnika Sinha <varnsinha@gmail.com>
2017-11-15 20:39:52 -0500

    final push

--- IMDBActorsGraph.java
+++ IMDBActorsGraph.java
@@ -5,36 +5,36 @@ import java.util.Map;
 
 public class IMDBActorsGraph extends IMDBGraph implements Graph {
 
-    final private Map<String, ActorNode> _listOfActors;
+	final private Map<String, ActorNode> _listOfActors;
 
-    /**
-     * Constructor
-     * @param actorsFilename the filename of the actors
-     * @param actressesFilename the filename of the actresses
-     * @throws IOException if the file is not found
-     */
-    public IMDBActorsGraph(String actorsFilename, String actressesFilename) throws IOException {
-        super(actorsFilename, actressesFilename);
-        _listOfActors = actors;
-    }
+	/**
+	 * Constructor
+	 * @param actorsFilename the filename of the actors
+	 * @param actressesFilename the filename of the actresses
+	 * @throws IOException if the file is not found
+	 */
+	public IMDBActorsGraph(String actorsFilename, String actressesFilename) throws IOException {
+		super(actorsFilename, actressesFilename);
+		_listOfActors = actors;
+	}
 
-    /**
+	/**
 	 * Returns a collection of all the nodes in the graph
 	 * @return a collection of all the nodes in the graph
 	 */
-    @Override
-    public Collection<ActorNode> getNodes() {
-        return new ArrayList<>(actors.values());
-    }
-    
-    /**
+	@Override
+	public Collection<ActorNode> getNodes() {
+		return new ArrayList<>(actors.values());
+	}
+
+	/**
 	 * Returns the node of the specified name or null if no such node exists
 	 * @param name the name of the specified node
 	 * @return the node associated with the specified name or null
 	 * if no such node exists.
 	 */
-    @Override
-    public ActorNode getNodeByName(String name) {
-        return _listOfActors.get(name);
-    }
+	@Override
+	public ActorNode getNodeByName(String name) {
+		return _listOfActors.get(name);
+	}
 }
